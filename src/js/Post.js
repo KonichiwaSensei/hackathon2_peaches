b009ee0ef662a1198076f72a900a89ac7592c9bc
Matthew Shenar <matthew.shenar@gmail.com>
2023-05-19 14:55:27 +0200

    working submit

--- src/js/Post.js
+++ src/js/Post.js
@@ -1,21 +1,51 @@
-export const submitRegistrationData = async () => {
+export const submitRegistrationData = () => {
 
     const url = `https://test-api.codingbootcamp.cz/api/a0f7802f/events/1/registrations`
-  
-    const myDataObject = {
-      "First Name": "Rex",
-      "Last Name": "Goodboi",
-      "Phone Number": "42069",
-      "email": "matthew.shenar@gmail.com",
-      "breed": "german shepherd"
-    }
-    const myResponse = await fetch(url, {
-      "method": "POST",
-      "body": JSON.stringify(myDataObject),
-      "headers": {
-        'Content-Type': 'application/json'
-      }
+    
+    const firstName = document.querySelector('#fname')
+    const lastName = document.querySelector('#lname')
+    const userEmail = document.querySelector('#email')
+    const userPhone = document.querySelector('#pnumber')
+    const dateOfBirth = document.querySelector('#dateb')
+    const submitButtonRequest = document.querySelector('#subbtn')
+    const registrationForm = document.querySelector('#registration')
+
+    console.log(submitButtonRequest);
+
+
+    registrationForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        // console.log({
+        //     "First Name": firstName.value,
+        //     "Last Name": lastName.value,
+        //     "email": userEmail.value,
+        //     "Phone Number": userPhone.value,
+        //     "Date Of Birth": dateOfBirth.value
+        //   });
+        // console.log("boo");
+        const myDataObject = {
+            "First Name": firstName.value,
+            "Last Name": lastName.value,
+            "email": userEmail.value,
+            "Phone Number": userPhone.value,
+            "Date Of Birth": dateOfBirth.value
+          }
+
+          const myResponse = await fetch(url, {
+            "method": "POST",
+            "body": JSON.stringify(myDataObject),
+            "headers": {
+              'Content-Type': 'application/json'
+            }
+          })
+
+          console.log(myResponse);
+          const myUsableResponse = await myResponse.json()
+          console.log(myUsableResponse)
+
     })
-    const myUsableResponse = await myResponse.json()
-    console.log(myUsableResponse)
-  }
+
+
+
+
+    }
