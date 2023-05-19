89e0fbad290b508336606693bbdf977c5ba8de6a
GoncaloRodrigues <goncasrod2000@gmail.com>
2023-05-19 13:11:39 +0200

    modal created

---
+++ src/js/Modal.js
@@ -0,0 +1,29 @@
+// Get the modal
+const modal = document.querySelector(".modal-content");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log(modal);
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  
+}
+
+
